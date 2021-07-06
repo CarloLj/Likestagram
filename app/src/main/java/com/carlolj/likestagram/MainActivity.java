40ffe8258d30f0345e8c0ebd754dfed4427dfd32
Carlo Angel Luján García <carlouxljlj@gmail.com>
2021-07-06 17:44:49 -0500

    User can log out

--- app/src/main/java/com/carlolj/likestagram/MainActivity.java
+++ app/src/main/java/com/carlolj/likestagram/MainActivity.java
@@ -2,7 +2,11 @@ package com.carlolj.likestagram;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.parse.ParseUser;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +15,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void onLogout(View view) {
+        onLogoutButton();
+    }
+
+    public void onLogoutButton() {
+        // forget who's logged in
+        ParseUser.logOut();
+
+        // navigate backwards to Login screen
+        Intent i = new Intent(this, LoginActivity.class);
+        i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP); // this makes sure the Back button won't work
+        i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK); // same as above
+        startActivity(i);
+    }
+
+
 }
