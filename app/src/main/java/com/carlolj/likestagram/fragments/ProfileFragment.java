cbd17b14e5cc10113240a7e26c57a85c9a4c8dc7
Carlo Angel Luján García <carlouxljlj@gmail.com>
2021-07-09 19:10:31 -0500

    User can change the profile picture

--- app/src/main/java/com/carlolj/likestagram/fragments/ProfileFragment.java
+++ app/src/main/java/com/carlolj/likestagram/fragments/ProfileFragment.java
@@ -1,7 +1,11 @@
 package com.carlolj.likestagram.fragments;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.ImageDecoder;
 import android.media.Image;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -11,6 +15,7 @@ import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -18,9 +23,11 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.resource.bitmap.CircleCrop;
+import com.carlolj.likestagram.BitmapScaler;
 import com.carlolj.likestagram.LoginActivity;
 import com.carlolj.likestagram.adapters.ProfileAdapter;
 import com.carlolj.likestagram.models.Post;
@@ -29,22 +36,27 @@ import com.carlolj.likestagram.R;
 import com.parse.FindCallback;
 import com.parse.Parse;
 import com.parse.ParseException;
+import com.parse.ParseFile;
 import com.parse.ParseQuery;
 import com.parse.ParseUser;
+import com.parse.SaveCallback;
 
 import org.jetbrains.annotations.NotNull;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class ProfileFragment extends Fragment {
 
+    public final static int PICK_PHOTO_CODE = 1046;
     public static final String TAG = "HomeFragment";
     private RecyclerView rvPosts;
     private SwipeRefreshLayout swipeContainer;
     private ImageView ivProfileImage;
     private TextView tvUsername;
-    private Button btnLogOut;
+    private Button btnLogOut,btnChangePicture;
     protected ProfileAdapter adapter;
     protected List<Post> allPosts;
     private ParseUser user;
@@ -71,6 +83,7 @@ public class ProfileFragment extends Fragment {
         ivProfileImage = view.findViewById(R.id.ivProfileImage);
         tvUsername = view.findViewById(R.id.tvUsername);
         btnLogOut = view.findViewById(R.id.btnLogOut);
+        btnChangePicture = view.findViewById(R.id.btnChangePicture);
 
         tvUsername.setText(user.getUsername());
         Glide.with(getContext())
@@ -96,10 +109,17 @@ public class ProfileFragment extends Fragment {
 
         if (ParseUser.getCurrentUser() == user) {
             btnLogOut.setVisibility(View.VISIBLE);
+            btnChangePicture.setVisibility(View.VISIBLE);
             btnLogOut.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
                     onLogout();
+                }
+            });
+            btnChangePicture.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    onPickPhoto(v);
                 }
             });
         } else {
@@ -145,5 +165,69 @@ public class ProfileFragment extends Fragment {
         i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP); // this makes sure the Back button won't work
         i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK); // same as above
         startActivity(i);
+    }
+
+    // Trigger gallery selection for a photo
+    public void onPickPhoto(View view) {
+        // Create intent for picking a photo from the gallery
+        Intent intent = new Intent(Intent.ACTION_GET_CONTENT,
+                MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+
+        // If you call startActivityForResult() using an intent that no app can handle, your app will crash.
+        // So as long as the result is not null, it's safe to use the intent.
+        if (intent.resolveActivity(getActivity().getPackageManager()) != null) {
+            // Bring up gallery to select a photo
+            startActivityForResult(intent, PICK_PHOTO_CODE);
+        }
+    }
+
+    public Bitmap loadFromUri(Uri photoUri) {
+        Bitmap image = null;
+        try {
+            // check version of Android on device
+            if(Build.VERSION.SDK_INT > 27){
+                // on newer versions of Android, use the new decodeBitmap method
+                ImageDecoder.Source source = ImageDecoder.createSource(getActivity().getContentResolver(), photoUri);
+                image = ImageDecoder.decodeBitmap(source);
+            } else {
+                // support older versions of Android by using getBitmap
+                image = MediaStore.Images.Media.getBitmap(getActivity().getContentResolver(), photoUri);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return image;
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if ((data != null) && requestCode == PICK_PHOTO_CODE) {
+            Uri photoUri = data.getData();
+
+            // Load the image located at photoUri into selectedImage
+            Bitmap selectedImage = loadFromUri(photoUri);
+            Bitmap resizedBitmap = BitmapScaler.scaleToFitWidth(selectedImage, 500);
+
+            ByteArrayOutputStream stream = new ByteArrayOutputStream();
+            resizedBitmap.compress(Bitmap.CompressFormat.JPEG, 100, stream);
+
+            byte[] byteArray = stream.toByteArray();
+
+            ParseFile newProfileImage = new ParseFile("updated.png", byteArray);
+
+            user.put("profilePicture", newProfileImage);
+            user.saveInBackground(new SaveCallback() {
+                @Override
+                public void done(ParseException e) {
+                    if (e!=null) {
+                       Log.e(TAG, "Profile image wasn't updated", e);
+                       return;
+                    }
+                    Toast.makeText(getContext(), "New profile picture!", Toast.LENGTH_SHORT).show();
+                }
+            });
+            // Load the selected image into a preview
+            Glide.with(getContext()).load(byteArray).circleCrop().into(ivProfileImage);
+        }
     }
 }
